74bfc7a7dba13fa7efcc0e6fb13f665ead2843ad
MD. Yasin Ali Khan <shihabyasin@gmail.com>
2019-07-09 11:00:29 +0600

    c
    c

--- python_venv_create.sh
+++ python_venv_create.sh
@@ -6,7 +6,13 @@ source venvpy3/bin/activate
 
 
 # install all default requirements
+python3 -m pip install --ignore-installed -r requirements.txt
 sudo pip3 install -r requirements.txt
 
 # with system package
+sudo python3 -m venv --system-site-packages $pwd venvpy3
 virtualenv --system-site-packages
+
+
+
+
