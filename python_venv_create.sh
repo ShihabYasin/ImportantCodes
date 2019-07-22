09545d90ce5f497b2b806eb1244e1f30d0fe8dd6
MD. Yasin Ali Khan <shihabyasin@gmail.com>
2019-07-22 00:33:29 -0400

    c
    c

--- python_venv_create.sh
+++ python_venv_create.sh
@@ -17,3 +17,28 @@ virtualenv --system-site-packages
 
 
 
+<script src="https://gist.github.com/Geoyi/d9fab4f609e9f75941946be45000632b.js"></script>
+
+How to install virtualenv:
+Install pip first
+sudo apt-get install python3-pip
+Then install virtualenv using pip3
+sudo pip3 install virtualenv 
+Now create a virtual environment
+virtualenv venv 
+you can use any name insted of venv
+
+You can also use a Python interpreter of your choice
+virtualenv -p /usr/bin/python2.7 venv
+Active your virtual environment:
+source venv/bin/activate
+Using fish shell:
+source venv/bin/activate.fish
+To deactivate:
+deactivate
+Create virtualenv using Python3
+virtualenv -p python3 myenv
+Instead of using virtualenv you can use this command in Python3
+python3 -m venv myenv
+
+
