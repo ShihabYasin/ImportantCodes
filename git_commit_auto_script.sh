f1f74f69e556026e7e90199d31118968277b8a58
yasin <yasin@tigerit.com>
2020-11-15 18:05:36 +0600

    currentDate

--- git_commit_auto_script.sh
+++ git_commit_auto_script.sh
@@ -1,19 +1,22 @@
-#!/bin/bash  
+#!/bin/bash
 ## git upload auto script - example script
-## CLASS_PROGRAMMING 
 
 ## Pass repo_path as 1st argument
+# example usage: sudo ./git_commit_auto_script.sh  https://github.com/<UserName>/<RepoName>
+
+if [ "$0" == "-h" ]; then
+  echo "Usage: `sudo ./git_commit_auto_script.sh $repolink` [somestuff]"
+  return
+fi
 
 PATH_REPO=$1
-
 
 cd $PATH_REPO
 git add .
 
-currentDate=`date +"%Y-%m-%d %T"`
-git commit -m  currentDate
-git pull origin master 
-git push origin master 
-
+currentDate=$(date +"%Y-%m-%d %T")
+git commit -m currentDate
+git pull origin master
+git push origin master
 
 exit 1
