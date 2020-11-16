3f17a9a66b46cd402fdc3f87b405422e91f24fe3
yasin <yasin@tigerit.com>
2020-11-16 06:38:12 +0600

    noise_remove from wav file utility adding.

---
+++ kaldi/Utility/noise_remove.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Give sample noise in dir "noise_sound", this .sh will generate clean wavs in dir "wav_decode" .
+# Output file format: {prev_name}_clean.wav
+
+FILE="noise_sound/noise.wav"
+DIR="noise_sound"
+
+if [ "$(ls -A $DIR)" ]; then
+	if [ -f "$FILE" ]; then
+		echo "Noise Reduction Applied."
+		#Generate a noise profile in sox:
+		sox noise_sound/noise.wav -n noiseprof noise.prof
+		#Clean the noise from the audio		
+		for f in wav_decode/*.wav; do
+			sox "$f" "$f"_clean.wav noisered noise.prof 0.21  # Noise Reduction Threshold 
+		done
+	else 
+		echo "No Noise Reduction. $FILE does not exist to do Noise Reduction."
+	fi 
+fi
+
+exit 1
